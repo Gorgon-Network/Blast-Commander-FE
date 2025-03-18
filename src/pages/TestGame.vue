4bfbcea6babbe86be4542ac9632ab8abc25c5167
Hien <hackernovirus@gmail.com>
2025-03-19 00:15:12 +0700

    connect unity

--- src/pages/TestGame.vue
+++ src/pages/TestGame.vue
@@ -48,7 +48,9 @@ const handleGameMessage = async (event) => {
   switch (event.data.type) {
     case "connectWallet":
       console.log("🔗 Đang gửi sự kiện connectWallet đến Unity...");
-      unityInstance.SendMessage("WebGLEvents", "SetConnectedWallet", "0xA03a73B343799814013524A0857f3805Bc372F39");
+      const {signer} = await _connectWallet();
+      console.log(111, signer);
+      unityInstance.SendMessage("WebGLEvents", "SetConnectedWallet", signer.address);
       break;
 
     case "mintNFTSuccess":
